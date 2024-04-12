15ed0d9a7ba15ddff90210c850783de8513b5fd0
Vicente Viera <plantedcoot706@gmail.com>
2024-04-11 19:26:25 -0600

    First Commit

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <config.hpp>
+
+int main(int argc, char** argv){
+    std::cout<<project_name<<std::endl;
+    std::cout<<project_version<<std::endl;
+
+    return 0;
+}
